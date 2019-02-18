d42b25ff875fc6cbe4b7583665ed8185844f3a02
devayanip <pawardevayani20@gmail.com>
2019-02-18 15:01:57 +0530

    fcommit2

---
+++ src/main/java/com/test2/Jenkinsdemo/MyCommonUtility.java
@@ -0,0 +1,34 @@
+package com.test2.Jenkinsdemo;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class MyCommonUtility 
+{
+	WebDriver driver;
+
+	
+	public MyCommonUtility(WebDriver driver) {
+		this.driver=driver;
+	}
+
+	public String screenshot(String nameOfScreenshot)
+	{
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String dest = System.getProperty("user.dir") +"\\Screens\\"+nameOfScreenshot+".png";
+		File destination = new File(dest);
+		try {
+			FileUtils.copyFile(source, destination);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dest;
+	}
+}
